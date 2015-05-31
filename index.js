b04bd470e1ff463fc8cc17848e4921a797f4e63b
anvaka <anvaka@gmail.com>
2015-05-31 01:39:54 -0700

    Addded line drawing

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var THREE = require('three');
 var combineOptions = require('./options.js');
 var createParticleView = require('./lib/particle-view.js');
+var createLineView = require('./lib/line-view.js');
 var createHitTest = require('./lib/hit-test.js');
 var flyControls = require('three.fly');
+var normalizeColor = require('./lib/normalize-color.js');
 
 // Expose three.js as well, so simple clients do not have to require it
 unrender.THREE = THREE;
@@ -16,7 +18,9 @@ function unrender(container, options) {
     camera: getCamera,
     renderer: getRenderer,
     particles: particles,
-    hitTest: getHitTest
+    hitTest: getHitTest,
+    highlight: highlight,
+    lines: drawLines
   };
 
   options = combineOptions(options);
@@ -26,6 +30,7 @@ function unrender(container, options) {
   var camera = createCamera();
   var renderer = createRenderer();
   var particleView = createParticleView(scene);
+  var lineView = createLineView(scene);
   var input = createInputHandler();
 
   // TODO: This doesn't seem to belong here... Not sure where to put it
@@ -39,6 +44,13 @@ function unrender(container, options) {
 
   function getHitTest() {
     return hitTest;
+  }
+
+  function highlight(indexes, color) {
+    color = normalizeColor(color);
+    if (!color) color = 0xff0000;
+
+    particleView.highlight(indexes, color);
   }
 
   function createInputHandler() {
@@ -112,105 +124,8 @@ function unrender(container, options) {
 
   function startEventsListening() {
     window.addEventListener('resize', onWindowResize, false);
-    hitTest.on('highlight', particleView.highlight);
-
-    //hitTest.on('rendertree', renderTree);
   }
 
-  function renderTree(tree) {
-    var treeGeometry = new THREE.BufferGeometry();
-    var material = new THREE.LineBasicMaterial({
-      vertexColors: THREE.VertexColors
-    });
-    var octs = createOcts(tree);
-
-    var points = new Float32Array(octs);
-    var colors = createColors(points.length);
-
-    treeGeometry.addAttribute('position', new THREE.BufferAttribute(points, 3));
-    treeGeometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
-    var edgeMesh = new THREE.Line(treeGeometry, material, THREE.LinePieces);
-    edgeMesh.frustumCulled = false;
-    scene.add(edgeMesh);
-  }
-
-  function createOcts(tree) {
-    var points = [];
-    var root = tree.getRoot();
-    addNode(points, root);
-    return points;
-  }
-
-  function addNode(points, node) {
-    if (!node) return;
-    addBounds(points, node.bounds);
-    addNode(points, node.q0);
-    addNode(points, node.q1);
-    addNode(points, node.q2);
-    addNode(points, node.q3);
-    addNode(points, node.q4);
-    addNode(points, node.q5);
-    addNode(points, node.q6);
-    addNode(points, node.q7);
-  }
-
-  function createColors(length) {
-    var arr = new Float32Array(length);
-    for (var i = 0; i < length; ++i) {
-      arr[i] = 0.5;
-    }
-    return arr;
-  }
-
-  function addBounds(to, bounds) {
-    var left = bounds.x - bounds.half;
-    var right = bounds.x + bounds.half;
-    var top = bounds.y - bounds.half;
-    var bottom = bounds.y + bounds.half;
-    var back = bounds.z - bounds.half;
-    var front = bounds.z + bounds.half;
-
-    // front
-    to.push(left, top, front,
-            right, top, front,
-
-            right, top, front,
-            right, bottom, front,
-
-            right, bottom, front,
-            left, bottom, front,
-
-            left, bottom, front,
-            left, top, front);
-
-    // back:
-    to.push(left, top, back,
-            right, top, back,
-
-            right, top, back,
-            right, bottom, back,
-
-            right, bottom, back,
-            left, bottom, back,
-
-            left, bottom, back,
-            left, top, back);
-
-    // bottom
-    to.push(left, bottom, back,
-            left, bottom, front,
-
-            right, bottom, back,
-            right, bottom, front
-            );
-    // top
-    to.push(left, top, back,
-            left, top, front,
-
-            right, top, back,
-            right, top, front
-            );
-  }
 
   function stopEventsListening() {
     window.removeEventListener('resize', onWindowResize, false);
@@ -221,5 +136,9 @@ function unrender(container, options) {
     camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(container.clientWidth, container.clientHeight);
+  }
+
+  function drawLines(lines) {
+    lineView.draw(lines);
   }
 }
